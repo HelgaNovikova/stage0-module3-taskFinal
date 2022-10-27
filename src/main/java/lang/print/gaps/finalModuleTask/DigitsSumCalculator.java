fbc5b6c13edeeaf5e81acff3ac79d2ff8943633b
HelgaNovikova <helpotaki@rambler.ru>
2022-10-27 14:13:19 -0400

    task3.6

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int thousands = (int) number / 1000;
+        int hundreds = (int) (number - thousands * 1000) / 100;
+        int dozens = (int) (number - thousands * 1000 - hundreds * 100) / 10;
+        int units = (int) (number - thousands * 1000 - hundreds * 100 - dozens * 10);
+        System.out.println(thousands+ hundreds + dozens + units);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int hundreds = (int) number / 100;
+        int dozens = (int) (number - hundreds * 100) / 10;
+        int units = (int) (number - hundreds * 100 - dozens * 10);
+        int newNumber = units * 100 + dozens * 10 + hundreds;
+        System.out.println(newNumber);
     }
 }
